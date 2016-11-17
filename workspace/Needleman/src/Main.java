f084ff84f1dd76e78af1f841d4f7e622ce2df5f9
user <user@Sanchez.fritz.box>
2016-11-17 21:03:47 +0100

    Added some Files

--- workspace/Needleman/src/Main.java
+++ workspace/Needleman/src/Main.java
@@ -1,10 +1,26 @@
+import javafx.application.*;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+public class Main extends Application{
 
-public class Main {
-
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo!");
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	    HBox root = new HBox(); 
+	    Scene scene = new Scene(root, 300, 250);
+	    primaryStage.setScene(scene);
+	    primaryStage.setTitle("Needleman-Wunsch-Visualizer");
+	    primaryStage.show();
+	    
+		
 	}
 
 }
---
+++ workspace/Needleman/src/guiElements/ControlNode.java
@@ -0,0 +1,16 @@
+package guiElements;
+
+import com.sun.javafx.geom.BaseBounds;
+import com.sun.javafx.geom.transform.BaseTransform;
+import com.sun.javafx.jmx.MXNodeAlgorithm;
+import com.sun.javafx.jmx.MXNodeAlgorithmContext;
+import com.sun.javafx.sg.prism.NGNode;
+
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+
+public class ControlNode extends Pane{
+
+	
+
+}
